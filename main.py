ab31d90501d8024e67432e0ec3b46500eb23139a
NoodMan <camara.adeline@yahoo.fr>
2022-05-18 14:53:26 +0200

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # # CODE TEST CONVERSION EN INT (ECHEC)
 # import csv # on import csv
-# from itertools import count  
+# from itertools import count
 
 # # fichier à lire en mode r (reading)
 # filename = r'export_contacts_1652774627_2.csv'
@@ -17,10 +17,10 @@
 #                 int(row[1]),
 #                 int(row[2]),
 #                 int(row[3]),
-               
+
 #             ])  # on ajoute chaque ligne dans le [] data
 #         count += 1
-        
+
 # for d in data:  # itération via le [] ligne par ligne
 
 #     print(d)  # impression du resultat ligne par ligne
@@ -30,30 +30,57 @@
 
 
 
-#CODE POUR LIRE ET IGNORER LES DEUX PERMIERE LIGNES (FONCTIONNE 🎉)
-import csv # on import csv
-from itertools import count  
 
+#CODE POUR LIRE UN FICHIER CSV SANS TABLEAU (Me donne uniquement la dernier ligne???What's!!!!!!)
+# import csv
 
-# fichier à lire en mode r (reading)
-filename = r'export_contacts_1652774627_2.csv'
-data = []  # on déclare un tableau vide pour les datas
+# sample_csv = 'export_contacts_1652774627_2.csv'
 
-with open(filename) as csvfile:  # on lit le fichier que je renomme avec un alias (as)
-    csvreader = csv.reader(csvfile, delimiter=',')
-    count = 0 # le compteur
-    for row in csvreader:
-        if count > 1: #on ignore les lignes avec le nom du fichier et les valeurs des colonnes= 1 / juste le nom =0...
-            # if row[0] != ' ':
-            data.append(row)  # on ajoute chaque ligne dans le [] data
-        count += 1 #on incremante de 1
-        
-    for d in data:  # itération via le [] ligne par ligne
-
-        print(d)  # impression du resultat ligne par ligne
+# with open(sample_csv, "r") as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     for line in csv_reader:
+#         count = 0
+#         if count > 1:
+#             count += 1
+# print(line)
 
 
 
 
+#CODE POUR ECRITE MAIS NE FONCTIONNE PAS
+# import csv
+
+# sample_csv = 'export_contacts_1652774627_2.csv'
+
+# with open(sample_csv, "r") as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+#     for line in csv_reader: 
+#         print("{name} habite {Address}"
+#               .format(name=line["Société"],
+#                       Address=line['Rue'],
+#                       ))
 
 
+
+
+# CODE POUR LIRE ET IGNORER LES DEUX PERMIERE LIGNES (FONCTIONNE 🎉)
+# import csv # on import csv
+# from itertools import count
+
+
+# # fichier à lire en mode r (reading)
+# filename = r'export_contacts_1652774627_2.csv'
+# data = []  # on déclare un tableau vide pour les datas
+
+# with open(filename) as csvfile:  # on lit le fichier que je renomme avec un alias (as)
+#     csvreader = csv.reader(csvfile, delimiter=',')
+#     count = 0 # le compteur
+#     for row in csvreader:
+#         if count > 1: #on ignore les lignes avec le nom du fichier et les entete des colonnes= 1 / juste le nom =0...
+#             # if row[0] != ' ':
+#             data.append(row)  # on ajoute chaque ligne dans le [] data
+#         count += 1 #on incremante de 1
+
+#     for d in data:  # itération via le [] ligne par ligne
+
+#         print(d)  # impression du resultat ligne par ligne
